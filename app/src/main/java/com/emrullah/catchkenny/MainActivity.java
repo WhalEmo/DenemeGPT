da590a5c2193913ffdd281d163f13e0bcf2de00f
WhalEmo <emrullah64uygun@gmail.com>
2024-12-01 20:07:38 +0300

    Starting

---
+++ app/src/main/java/com/emrullah/catchkenny/MainActivity.java
@@ -0,0 +1,124 @@
+package com.emrullah.catchkenny;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView timeText;
+    TextView scoreText;
+    int score;
+
+    ImageView image1;
+    ImageView image2;
+    ImageView image3;
+    ImageView image4;
+    ImageView image5;
+    ImageView image6;
+    ImageView image7;
+    ImageView image8;
+    ImageView image9;
+
+    ImageView[] imageArray;
+    Handler handler;
+    Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        image1 = findViewById(R.id.imageView4);
+        image2 = findViewById(R.id.imageView5);
+        image3 = findViewById(R.id.imageView6);
+        image4 = findViewById(R.id.imageView7);
+        image5 = findViewById(R.id.imageView8);
+        image6 = findViewById(R.id.imageView9);
+        image7 = findViewById(R.id.imageView10);
+        image8 = findViewById(R.id.imageView11);
+        image9 = findViewById(R.id.imageView12);
+        imageArray = new ImageView[]{image1, image2, image3, image4, image5, image6, image7, image8, image9};
+        timeText = findViewById(R.id.timeText);
+        scoreText = findViewById(R.id.scoreText);
+        score = 0;
+        hideImages();
+
+        new CountDownTimer(10000,1000){
+
+            @Override
+            public void onTick(long l) {
+                timeText.setText("Time: "+(l/1000));
+            }
+
+            @Override
+            public void onFinish() {
+                timeText.setText("Time off!!");
+                handler.removeCallbacks(runnable);
+                for(ImageView image : imageArray){
+                    image.setVisibility(View.INVISIBLE);
+                }
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Restart");
+                alert.setMessage("Are you restart game?");
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"Game Over!!!",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.show();
+            }
+        }.start();
+    }
+    public void scoreP(View view){
+        score++;
+        scoreText.setText("Score: "+score);
+    }
+
+    public void hideImages(){
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                for(ImageView image : imageArray){
+                    image.setVisibility(View.INVISIBLE);
+                }
+                Random random = new Random();
+                int i = random.nextInt(9);
+                imageArray[i].setVisibility(View.VISIBLE);
+                handler.postDelayed(runnable,500);
+            }
+        };
+        handler.post(runnable);
+    }
+
+}
